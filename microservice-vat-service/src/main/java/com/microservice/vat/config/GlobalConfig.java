9acc149ca0a60942c4588fa09870a56c9eb1d0c5
Sourav Kantha <souravkantha@Souravs-Air.Dlink>
2023-01-29 19:20:15 +0530

    Added Eureka Server

--- microservice-vat-service/src/main/java/com/microservice/vat/config/GlobalConfig.java
+++ microservice-vat-service/src/main/java/com/microservice/vat/config/GlobalConfig.java
@@ -1,12 +1,19 @@
 package com.microservice.vat.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.discovery.EurekaClient;
 
 @Configuration
 public class GlobalConfig {
+	
+	@Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
 	
 	@Bean
 	ObjectMapper jackson2ObjectMapperBuilder() {
