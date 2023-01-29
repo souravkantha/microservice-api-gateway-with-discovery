1716d02e3d2e02cf39b5ae11f9f8bad1559fa089
Sourav Kantha <souravkantha@Souravs-Air.Dlink>
2023-01-29 14:02:07 +0530

    added discovery

---
+++ microservice-discovery-server/src/main/java/com/mircoservice/discovery/MicroserviceDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.mircoservice.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ microservice-discovery-server/src/test/java/com/mircoservice/discovery/MicroserviceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircoservice.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
