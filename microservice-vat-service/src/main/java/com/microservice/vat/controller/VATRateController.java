feb304d1154ef92fdbf12f58f3f42c210aadcd54
Sourav Kantha <souravkantha@Souravs-Air.Dlink>
2023-01-30 00:01:58 +0530

    api gateway

--- microservice-vat-service/src/main/java/com/microservice/vat/controller/VATRateController.java
+++ microservice-vat-service/src/main/java/com/microservice/vat/controller/VATRateController.java
@@ -3,6 +3,7 @@ package com.microservice.vat.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,15 @@ import com.microservice.vat.dto.VATRate;
 import com.microservice.vat.service.VATRateService;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/vat/rates", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VATRateController {
+	
+	@Value("${server.port}")
+	private int serverPort;
 
 	@Autowired
 	private VATRateService vatRateService;
@@ -28,6 +34,7 @@ public class VATRateController {
 			@Parameter(description = "Optional, provide number of countries to be fetched")  @RequestParam(name = "count", required = false, defaultValue = "3") Integer count ) {
 		
 		
+		log.info("Serving from port : " + serverPort);
 		return vatRateService.getHighestStandardVATRates();
 		
 	}
